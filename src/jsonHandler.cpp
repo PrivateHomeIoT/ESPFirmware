0045dfd9b245796e18c14f181cd707d43c8c74c8
Maximilian Inckmann <maximilianinck@icloud.com>
2020-12-29 13:38:37 +0100

    switched to another (better) JSON lib

--- src/jsonHandler.cpp
+++ src/jsonHandler.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include <ArduinoJson.h>
+#include <Arduino_Json.h>
 #include <LittleFS.h>
 #include <EEPROM.h>
 #include "jsonHandler.h"
@@ -42,10 +42,10 @@ Function functions[] = {Function(), Function((char*)"Input")};
 
 struct Port
 {
-    char *pin;
+    int pin;
     Function type;
 
-    Port(char *port, int function)
+    Port(int port, int function)
     {
         pin = port;
         type = functions[function];
@@ -69,17 +69,14 @@ int getPosition(Function p)
 
 void parsePorts(String rawJSON)
 {
-    const size_t capacity = JSON_ARRAY_SIZE(64) + JSON_OBJECT_SIZE(1) + 64 * JSON_OBJECT_SIZE(2) + 40;
-    DynamicJsonDocument doc(capacity);
-    deserializeJson(doc, rawJSON);
-    JsonArray ports = doc["ports"];
+    JSONVar ports = JSON.parse(rawJSON)["ports"];
     int next = 0;
 
     for (int i = 0; i < 64; i++)
     {
         if (ports[i]["port"] != "")
         {
-            configuredPorts[next].pin = ports[i]["port"];
+            configuredPorts[next].pin = (int) ports[i]["port"];
             configuredPorts[next].type = functions[int(ports[i]["function"])];
             next++;
         }
@@ -88,23 +85,16 @@ void parsePorts(String rawJSON)
 
 String encodePorts()
 {
-    const size_t capacity = JSON_ARRAY_SIZE(64) + JSON_OBJECT_SIZE(1) + 64 * JSON_OBJECT_SIZE(2);
-    DynamicJsonDocument doc(capacity);
-
-    JsonArray ports = doc.createNestedArray("ports");
-
-    JsonObject ports_0 = ports.createNestedObject();
-    ports_0["port"] = "D1";
-    ports_0["function"] = 1;
+    JSONVar ports;
+    ports[0]["port"] = 1;
+    ports[0]["function"] = 1;
 
     for (int i = 0; i < 64; i++)
     {
         ports[i]["port"] = configuredPorts[i].pin;
         ports[i]["function"] = getPosition(configuredPorts[i].type);
     }
-    String result;
-    serializeJson(doc, result);
-    return result;
+    return ports.stringify();
 }
 
 void loadData()
