b363e02d4263e4257b168ac2171d2d0d9c51c49e
Maximilian Inckmann <maximilianinck@icloud.com>
2021-04-16 23:03:51 +0200

    minor fixes

--- src/handleEncryption.cpp
+++ src/handleEncryption.cpp
@@ -8,7 +8,6 @@ byte aes_key[] = {0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x
 
 // General initialization vector (you must use your own IV's in production for full security!!!)
 byte aes_iv[N_BLOCK] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-char base64_iv[16];
 
 void aes_init() {
   Serial.println("gen_iv()");
@@ -18,26 +17,21 @@ void aes_init() {
 void getNewIV(){
   byte result[16];
   char raw[16];
-  for(uint i = 0; i<16; i++){
-    result[i] = ESP8266TrueRandom.randomByte();
-    raw[i] = (char)result[i];
-    aes_iv[i] = result[i];
-  }
-  base64_encode(base64_iv, raw, 16);
+  for(uint i = 0; i<16; i++) aes_iv[i] = ESP8266TrueRandom.randomByte();
   aesLib.gen_iv(aes_iv);
 }
 
 char* p_encrypt(char * msg, uint16_t msgLen, byte iv[]) {
   int cipherlength = aesLib.get_cipher64_length(msgLen);
   char encrypted[cipherlength]; 
-  aesLib.encrypt64(msg, msgLen, encrypted, aes_key, sizeof(aes_key), iv);
+  aesLib.encrypt((byte *)msg, msgLen, encrypted, aes_key, sizeof(aes_key), iv);
   Serial.print("encrypted = "); Serial.println(encrypted);
   return encrypted;
 }
 
 char* p_decrypt(char * msg, uint16_t msgLen, byte iv[]) {
   char decrypted[msgLen];
-  aesLib.decrypt64(msg, msgLen, decrypted, aes_key, sizeof(aes_key), iv);
+  aesLib.decrypt((byte *)msg, msgLen, decrypted, aes_key, sizeof(aes_key), iv);
   return decrypted;
 }
 
