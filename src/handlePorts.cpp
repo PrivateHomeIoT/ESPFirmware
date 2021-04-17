fe9d32fbd36250cd7cc607c27b0ae3fbe8fcd569
Maximilian Inckmann <maximilianinck@icloud.com>
2021-04-17 21:30:28 +0200

    fixed minor errors

--- src/handlePorts.cpp
+++ src/handlePorts.cpp
@@ -7,9 +7,7 @@ uint8_t outputs[64];
 uint8_t inputs[64];
 
 bool isOutput(uint8_t port){
-    for(uint i = 0; i< sizeof(outputs); i++){
-        if(outputs[i] == port) return true;
-    }
+    for(uint i = 0; i< sizeof(outputs); i++) if(outputs[i] == port) return true;
     return false;
 }
 
@@ -17,11 +15,7 @@ void actPort(char* msg){
     JsonObject data = parseJSON(msg);
     uint8_t port = (int)(data["pin"]);
     uint8_t value = (int)(data["value"]);
-    if(isOutput(port)){
-        if(value == 0) digitalWrite(port, LOW);
-        else if(value == 1024) digitalWrite(port, HIGH);
-        else analogWrite(port, value);
-    }
+    actPort(port, value);
 }  
 
 void actPort(uint8_t port, uint8_t value){
